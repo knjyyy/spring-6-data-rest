4a68c71df96a002f2fe7e1394fc8cb12ba3f0077
kenju <rzzlknjtrndd@gmail.com>
2023-04-10 00:49:38 +0800

    Initial commit, sample code for Spring Data Rest

---
+++ src/main/java/com/spring/spring6datarest/domain/BeerStyleEnum.java
@@ -0,0 +1,6 @@
+package com.spring.spring6datarest.domain;
+
+public enum BeerStyleEnum {
+
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
---
+++ src/main/java/com/spring/spring6datarest/repository/BeerRepository.java
@@ -0,0 +1,21 @@
+package com.spring.spring6datarest.repository;
+
+import com.spring.spring6datarest.domain.Beer;
+import com.spring.spring6datarest.domain.BeerStyleEnum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.UUID;
+
+@RepositoryRestResource(path = "beer", collectionResourceRel = "beer")
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
+    Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
+
+    Page<Beer> findAllByBeerStyle(BeerStyleEnum beerStyle, Pageable pageable);
+
+    Page<Beer> findAllByBeerNameAndBeerStyle(String beerName, BeerStyleEnum beerStyle, Pageable pageable);
+
+    Beer findByUpc(String upc);
+}
